ef23979db79f955b8b9b78a62d0e04906dc1d913
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2020-05-23 23:59:22 +0200

    implementing few Genetic Algorithm Methods

--- GeneticAlgorithm/GA.cs
+++ GeneticAlgorithm/GA.cs
@@ -10,5 +10,27 @@ namespace GeneticAlgorithm
     public class GA
     {
         public BitArray[,,] chromosomes { get; set; }
+
+        public static int binary2Int(BitArray bitArray)
+        {
+            int result=0;
+            for(int index=0; index<bitArray.Length; index++)
+            {
+                int value = bitArray[index] ? 1 : 0;
+                result += (int)Math.Pow(2, bitArray.Length - 1 - index) * value;
+            }
+            return result;
+        }
+
+        public static BitArray int2Binary(int value)
+        {
+            return new BitArray(new int[] { value });
+        }
+
+        public static int generateRandomValue(int precision)
+        {
+            Random random = new Random();
+            return random.Next(0, (int)Math.Pow(2, precision));
+        }
     }
 }
