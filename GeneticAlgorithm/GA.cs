c85d37c4eb2f8cb0ec70d1851bb0e94c9550c1a7
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2020-05-24 11:47:40 +0200

    Expand GA functions

--- GeneticAlgorithm/GA.cs
+++ GeneticAlgorithm/GA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using org.mariuszgromada.math.mxparser;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,33 @@ namespace GeneticAlgorithm
 {
     public class GA
     {
-        public BitArray[,,] chromosomes { get; set; }
-        public float[,,] chromosomeValues;
+        public GA(AlgorithmParameters algorithmParameters, EvaluatedFunction evaluatedFunction)
+        {
+            this.algorithmParameters = algorithmParameters;
+            this.evaluatedFunction = evaluatedFunction;
+            this.Chromosomes = new BitArray[algorithmParameters.Generations, 2, algorithmParameters.Population];
+            this.ChromosomeValues = new float[algorithmParameters.Generations, 2, algorithmParameters.Population];
+
+            this.BestValues = new float[algorithmParameters.Generations, 3];
+            this.MeanValues = new float[algorithmParameters.Generations, 3];
+            this.MedianValues = new float[algorithmParameters.Generations, 3];
+
+        }
+
+        public BitArray[,,] Chromosomes { get; set; } // generation, {x,y}, population
+        public float[,,] ChromosomeValues { get; set; } // generation, {x,y}, value
+        public float[,] BestValues { get; set; } // generation, {x, y, f(x,y)}
+        public float[,] MeanValues { get; set; } // generation, {x, y, f(x,y)}
+        public float[,] MedianValues { get; set; } // generation, {x, y, f(x,y)}
+
         public AlgorithmParameters algorithmParameters { get; }
         public EvaluatedFunction evaluatedFunction { get; }
 
+
         public static int binary2Int(BitArray bitArray)
         {
-            int result=0;
-            for(int index=0; index<bitArray.Length; index++)
+            int result = 0;
+            for (int index = 0; index < bitArray.Length; index++)
             {
                 int value = bitArray[index] ? 1 : 0;
                 result += (int)Math.Pow(2, bitArray.Length - 1 - index) * value;
@@ -30,18 +49,77 @@ namespace GeneticAlgorithm
             return new BitArray(new int[] { value });
         }
 
-        public static int generateRandomValue(int precision)
+        public int generateRandomValue()
         {
             Random random = new Random();
-            return random.Next(0, (int)Math.Pow(2, precision));
+            return random.Next(0, (int)Math.Pow(2, algorithmParameters.Precision));
         }
 
-        public double[] relaxationFuncion (int xValue, int yValue)
+        public double[] relaxationFuncion(int xValue, int yValue)
         {
             double[] results = new double[2];
-            results[0] = (xValue * (evaluatedFunction.xDomain.Y - evaluatedFunction.xDomain.X) / Math.Pow(2, algorithmParameters.Precision))+evaluatedFunction.xDomain.X;
+            results[0] = (xValue * (evaluatedFunction.xDomain.Y - evaluatedFunction.xDomain.X) / Math.Pow(2, algorithmParameters.Precision)) + evaluatedFunction.xDomain.X;
             results[1] = (yValue * (evaluatedFunction.yDomain.Y - evaluatedFunction.yDomain.X) / Math.Pow(2, algorithmParameters.Precision)) + evaluatedFunction.yDomain.X;
             return results;
+        }
+
+        public double fitnessFunction(double xValue, double yValue)
+        {
+            Argument x = new Argument("x = " + xValue.ToString());
+            Argument y = new Argument("y = " + yValue.ToString());
+            Expression result = new Expression("f(x,y)", evaluatedFunction.function, x, y);
+            return result.calculate();
+        }
+
+        public BitArray mutation(BitArray chromosome)
+        {
+            Random random = new Random();
+            double drawnValue = random.NextDouble();
+
+            if (drawnValue < algorithmParameters.MutationProbability)
+            {
+                int mutationIndex = random.Next(0, chromosome.Length - 1);
+                chromosome[mutationIndex] = !chromosome[mutationIndex];
+            }
+            return chromosome;
+        }
+
+        public BitArray[] crossover(BitArray firstParent, BitArray secondParent)
+        {
+            Random random = new Random();
+            double drawnValue = random.NextDouble();
+            BitArray firstOffspring = new BitArray(firstParent.Length);
+            BitArray secondOffspring = new BitArray(secondParent.Length);
+
+            if (drawnValue < algorithmParameters.CrossoverProbability)
+            {
+                int crossoverIndex = random.Next(0, firstParent.Length - 1);
+
+                firstOffspring = CopySlice(firstParent, 0, crossoverIndex);
+                firstOffspring = CopySlice(secondParent, crossoverIndex, secondParent.Length - crossoverIndex);
+
+                secondOffspring = CopySlice(secondParent, 0, crossoverIndex);
+                secondOffspring = CopySlice(firstParent, crossoverIndex, firstParent.Length - crossoverIndex);
+            }
+            BitArray[] results = new BitArray[2];
+            results[0] = firstOffspring;
+            results[1] = secondOffspring;
+            return results;
+        }
+
+        public static BitArray CopySlice(BitArray source, int offset, int size)
+        {
+            BitArray results = new BitArray(source.Length);
+            for (int i = 0; i < size; i++)
+            {
+                results[offset + i] = source[offset + i];
+            }
+            return results;
+        }
+
+        public void Fit()
+        {
+
         }
     }
 }
