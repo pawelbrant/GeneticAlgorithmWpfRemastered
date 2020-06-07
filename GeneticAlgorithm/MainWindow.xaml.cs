f99bd929b4eac2dfa665bfac5afbbe9f93ed40d6
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2020-06-08 00:27:05 +0200

    Add style

--- GeneticAlgorithm/MainWindow.xaml.cs
+++ GeneticAlgorithm/MainWindow.xaml.cs
@@ -13,13 +13,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace GeneticAlgorithm
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         
         public MainWindow()
